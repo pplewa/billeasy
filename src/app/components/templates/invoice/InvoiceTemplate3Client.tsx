e7af8cf02445cf40734c31e22ca71eb367d536db
Peter Plewa <plewus@gmail.com>
2025-03-09 14:34:02 +1000

    checkpoint

---
+++ src/app/components/templates/invoice/InvoiceTemplate3Client.tsx
@@ -0,0 +1,5 @@
+'use client';
+
+// Re-export from our central template component
+import { Template3 } from './InvoiceTemplate3';
+export default Template3;
---
+++ src/app/components/templates/invoice/InvoiceTemplate4Client.tsx
@@ -0,0 +1,5 @@
+'use client';
+
+// Re-export from our central template component
+import { Template4 } from './InvoiceTemplate4';
+export default Template4;
