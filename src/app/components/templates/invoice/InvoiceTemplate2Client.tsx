a0041b963becd9e429c8461b1a5193c137dc7b14
Peter Plewa <plewus@gmail.com>
2025-03-10 16:39:55 +1000

    fixes

--- src/app/components/templates/invoice/InvoiceTemplate2Client.tsx
+++
@@ -1,5 +0,0 @@
-'use client';
-
-// Re-export from our central template component
-import { InvoiceTemplate2 } from './InvoiceTemplate';
-export default InvoiceTemplate2;
--- src/app/components/templates/invoice/InvoiceTemplate4Client.tsx
+++
@@ -1,5 +0,0 @@
-'use client';
-
-// Re-export from our central template component
-import { InvoiceTemplate4 } from './InvoiceTemplate';
-export default InvoiceTemplate4;
